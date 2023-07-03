890b83b09ce6a3ebcad55284bc8c27c86ecfc7bd
Bruno Riwerson Silva <riwerson@gmail.com>
2023-07-03 20:07:07 -0300

    fix: login button variable

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // O objeto validCredentials contém o e-mail e senha que devem
 // ser usados para efetuar login com sucesso nece desafio
-import validCredentials from "./validCredentials";
+const validCredentials = {
+  email: 'valid@email.com',
+  password: 'strongpassword',
+};
 
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
-const button = document.getElementById('button');
-
+const button = document.getElementById('login');
