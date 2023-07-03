b659f5d3ac6f0391e48918a8c9aeb2e460950c3d
Bruno Riwerson Silva <riwerson@gmail.com>
2023-07-03 19:41:18 -0300

    feat: defining variables to get form values

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // O objeto validCredentials contém o e-mail e senha que devem
 // ser usados para efetuar login com sucesso nece desafio
 import validCredentials from "./validCredentials";
+
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+const button = document.getElementById('button');
+
